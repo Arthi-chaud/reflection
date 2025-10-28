a7524139e06705b54af98f80e3caafc588a409f3
Arthur Jamet <arthur.jamet@gmail.com>
2025-10-28 11:38:37 +0000

    Reflection: Rust: Parser

--- benchmarks/rust/benches/data.rs
+++ benchmarks/rust/benches/data.rs
@@ -1,5 +1,16 @@
+use nom::{
+    IResult, Parser,
+    branch::alt,
+    bytes::{complete::tag, take_till},
+    character::digit1,
+    combinator::{map, map_res, opt},
+};
 use serde::{Deserialize, Serialize};
-use std::io::Write;
+use serde_json::from_str;
+use std::{
+    char,
+    io::{Error, Write},
+};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Book {
@@ -25,7 +36,7 @@ impl<W: Write> ReflSerialisable<W> for Book {
     fn serialise(&self, dest: &mut W) {
         let _ = write!(
             dest,
-            "{{\"title\":{},\"release_year\":{},\"page_count\":{},\"rating\":{}}}",
+            "{{\"title\":\"{}\",\"release_year\":{},\"page_count\":{},\"rating\":{}}}",
             self.title, self.release_year, self.page_count, self.rating
         );
         ()
@@ -35,9 +46,14 @@ impl<W: Write> ReflSerialisable<W> for Book {
 impl<W: Write, A: ReflSerialisable<W>> ReflSerialisable<W> for Vec<A> {
     fn serialise(&self, dest: &mut W) {
         let _ = write!(dest, "[");
+        let length = self.len();
+        let mut i = 0;
         for b in self {
             b.serialise(dest);
-            let _ = write!(dest, ",");
+            if i < length - 1 {
+                let _ = write!(dest, ",");
+            }
+            i = i + 1;
         }
         let _ = write!(dest, "]");
         ()
@@ -48,11 +64,98 @@ impl<W: Write> ReflSerialisable<W> for Author {
     fn serialise(&self, dest: &mut W) {
         let _ = write!(
             dest,
-            "{{\"name\":{},\"age\":{},\"books\":",
+            "{{\"name\":\"{}\",\"age\":{},\"books\":",
             self.name, self.age
         );
         self.books.serialise(dest);
         let _ = write!(dest, ",\"active\":{}}}", self.active);
         ()
+    }
+}
+
+pub trait ReflParsable<T> {
+    fn parse(str: &str) -> IResult<&str, T>;
+}
+
+impl ReflParsable<Book> for Book {
+    fn parse(str_: &str) -> IResult<&str, Book> {
+        let mut str = str_;
+        let mut book = Book {
+            title: String::new(),
+            release_year: 0,
+            page_count: 0,
+            rating: 0.0,
+        };
+
+        (str, _) = tag("{\"title\":\"")(str)?;
+        (str, book.title) = map_res(take_till(|c| c == '"'), |s: &str| {
+            Ok(s.to_string()) as Result<String, Error>
+        })
+        .parse(str)?;
+        (str, _) = tag("\",\"release_year\":")(str)?;
+        (str, book.release_year) = map_res(digit1(), |s| from_str::<u16>(s)).parse(str)?;
+        (str, _) = tag(",\"page_count\":")(str)?;
+        (str, book.page_count) = map_res(digit1(), |s| from_str::<u16>(s)).parse(str)?;
+
+        (str, _) = tag(",\"rating\":")(str)?;
+
+        (str, book.rating) =
+            map_res(take_till(|c: char| c == '}'), |s| from_str::<f32>(s)).parse(str)?;
+        let (str, _) = tag("}")(str)?;
+
+        Ok((str, book))
+    }
+}
+
+impl<A> ReflParsable<Vec<A>> for Vec<A>
+where
+    A: ReflParsable<A>,
+{
+    fn parse(str_: &str) -> IResult<&str, Vec<A>> {
+        let mut str = str_;
+        let mut res = Vec::with_capacity(100);
+        (str, _) = tag("[")(str)?;
+        loop {
+            match A::parse(str) {
+                Ok((str1, tmp)) => {
+                    res.push(tmp);
+                    (str, _) = opt(tag(",")).parse(str1)?;
+                }
+                _ => {
+                    (str, _) = tag("]").parse(str)?;
+                    return Ok((str, res));
+                }
+            }
+        }
+    }
+}
+
+impl ReflParsable<Author> for Author {
+    fn parse(str_: &str) -> IResult<&str, Author> {
+        let mut str = str_;
+        let mut author = Author {
+            name: String::new(),
+            age: 0,
+            active: false,
+            books: Vec::new(),
+        };
+
+        (str, _) = tag("{\"name\":\"")(str)?;
+        (str, author.name) = map_res(take_till(|c| c == '"'), |s: &str| {
+            Ok(s.to_string()) as Result<String, Error>
+        })
+        .parse(str)?;
+        (str, _) = tag("\",\"age\":")(str)?;
+        (str, author.age) = map_res(digit1(), |s| from_str::<u16>(s)).parse(str)?;
+        (str, _) = tag(",\"books\":")(str)?;
+        (str, author.books) = Vec::parse(str)?;
+
+        (str, _) = tag(",\"active\":")(str)?;
+
+        (str, author.active) =
+            map(alt((tag("true"), tag("false"))), |s: &str| s.eq("true")).parse(str)?;
+        let (str, _) = tag("}")(str)?;
+
+        Ok((str, author))
     }
 }
--- benchmarks/rust/benches/main.rs
+++ benchmarks/rust/benches/main.rs
@@ -1,22 +1,75 @@
 mod data;
 
-use std::io::BufWriter;
-
 use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-use crate::data::{Author, Book, ReflSerialisable};
+use crate::data::{Author, Book, ReflParsable, ReflSerialisable};
 
 fn serde_serialise<T: Serialize>(obj: &T) -> String {
     serde_json::to_string(obj).unwrap()
 }
 
-fn refl_serialise<T>(obj: &T) -> ()
+fn serde_parse<T>(s: &String) -> T
+where
+    T: for<'a> Deserialize<'a>,
+{
+    serde_json::from_str::<T>(s.as_str()).unwrap()
+}
+
+fn refl_serialise<T>(obj: &T) -> String
 where
     T: ReflSerialisable<Vec<u8>>,
 {
-    let mut buf = (Vec::with_capacity(1024));
-    obj.serialise(&mut buf)
+    let mut writer = Vec::with_capacity(128);
+
+    obj.serialise(&mut writer);
+    unsafe { String::from_utf8_unchecked(writer) }
+}
+
+fn refl_parse<T>(s: &String) -> ()
+where
+    T: ReflParsable<T>,
+{
+    T::parse(s.as_str()).unwrap();
+}
+
+pub fn parse_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("parse");
+    let book = Book {
+        title: "Hello World".to_owned(),
+        release_year: 2018,
+        page_count: 124,
+        rating: 0.8,
+    };
+    let obj = refl_serialise(&book);
+
+    // group.bench_with_input(BenchmarkId::new("object/refl", 0), &obj, |b, obj_| {
+    //     b.iter_with_large_drop(|| refl_parse::<Book>(obj_));
+    // });
+    // group.bench_with_input(BenchmarkId::new("object/serde", 0), &obj, |b, obj_| {
+    //     b.iter_with_large_drop(|| serde_parse::<Book>(obj_));
+    // });
+    //
+    for i in vec![1, 10, 50, 100].iter() {
+        let mut author = Author {
+            name: "John Doe".to_owned(),
+            age: 32,
+            active: true,
+            books: Vec::new(),
+        };
+        for _ in 0..(i.clone()) {
+            author.books.push(book.clone());
+        }
+        let bs = refl_serialise(&author);
+        group.bench_with_input(BenchmarkId::new("refl", i), &bs, |b, i_| {
+            b.iter_with_large_drop(|| refl_parse::<Author>(i_))
+        });
+
+        group.bench_with_input(BenchmarkId::new("serde", i), &bs, |b, i_| {
+            b.iter_with_large_drop(|| serde_parse::<Author>(i_))
+        });
+    }
+    group.finish();
 }
 
 pub fn serialise_benchmark(c: &mut Criterion) {
@@ -59,5 +112,5 @@ pub fn serialise_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, serialise_benchmark);
+criterion_group!(benches, parse_benchmark); // TODO Add serialiser
 criterion_main!(benches);
